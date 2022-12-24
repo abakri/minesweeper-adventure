bdee9a7299d7ec4741df41aa700734ca62b28671
Aslan <aslan.bakri@gmail.com>
2022-12-24 03:26:52 -0500

    Change max and min bombs

--- src/gameConfig.ts
+++ src/gameConfig.ts
@@ -2,8 +2,8 @@ const GameConfig = {
     WIDTH: 1000,
     HEIGHT: 1000,
     TILE_SIZE: 40,
-    MAX_BOMBS: 160,
-    MIN_BOMBS: 140,
+    MAX_BOMBS: 150,
+    MIN_BOMBS: 130,
     HERO_SPEED: 4,
 }
 
